5601495919a9fa8f0aeb3ff2827fc439731f6946
Aaron Kling <webgeek1234@gmail.com>
2019-08-19 18:21:44 -0500

    Initial commit
    This is a shim repo to android_device_nvidia_foster, where icosa
    is supported as part of the unified build. This will build without
    the Nvidia proprietary apps.

---
+++ lineage_icosa.mk
@@ -0,0 +1,8 @@
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit device configuration for icosa.
+$(call inherit-product, device/nintendo/icosa/full_icosa.mk)
+
+PRODUCT_NAME := lineage_icosa
+PRODUCT_DEVICE := icosa
